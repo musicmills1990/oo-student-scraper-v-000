460a535947bfeef29265f9519d00a6e7ea1f4a6f
Marc Mills <mdmills1990@gmail.com>
2018-09-26 14:20:54 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,7 @@ class Student
     student_hash.each do |attribute, value|
       self.send("#{attribute}=", value)
     end
-    @@all <<
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
